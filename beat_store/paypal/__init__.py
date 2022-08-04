306d9dfcb02d9ab85f5118b752b866f1c4528d33
Vince Maina <vchapandrews@gmail.com>
2022-08-04 13:18:40 +0100

    Quick test

--- beat_store/paypal/__init__.py
+++ beat_store/paypal/__init__.py
@@ -5,7 +5,8 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-PAYPAL_LIVE = os.environ['PAYPAL_LIVE']
+# PAYPAL_LIVE = os.environ['PAYPAL_LIVE']
+PAYPAL_LIVE = False
 PAYPAL_CLIENT_ID = os.environ['PAYPAL_CLIENT_ID']
 PAYPAL_CLIENT_SECRET = os.environ['PAYPAL_CLIENT_SECRET']
 
