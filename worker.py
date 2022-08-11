f773fdc890a2351c4cda32992bbea0a100070afe
Vince Maina <vchapandrews@gmail.com>
2022-08-11 20:20:13 +0100

    returning to the last version of this file that worked

--- worker.py
+++ worker.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 
 print('This file has been read.')
 
@@ -8,18 +8,6 @@ worker = Flask(__name__)
 def task_handler():
 
     print('Job was queued')
-
-    print('\nData:')
-    print(request.data)
-    
-    print('\nValues:')
     print(request.values)
 
-    print('\nGet Data:')
-    print(request.get_data(as_text=True))
-
-    print('\nGet json:')
-    print(request.get_json())
-
-    resp = jsonify(success=True)
-    return resp
+    return 'Job was queued - return'
