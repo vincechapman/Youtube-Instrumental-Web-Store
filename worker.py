28e893d889607fc55a026d01845042a737931867
Vince Maina <vchapandrews@gmail.com>
2022-08-11 19:36:39 +0100

    Adding prints to help with debugging

--- worker.py
+++ worker.py
@@ -8,6 +8,17 @@ worker = Flask(__name__)
 def task_handler():
 
     print('Job was queued')
+
+    print('\nData:')
+    print(request.data)
+    
+    print('\nValues:')
     print(request.values)
+
+    print('\nGet Data:')
+    print(request.get_data(as_text=True))
+
+    print('\nGet json:')
+    print(request.get_json())
 
     return 200
