5a24f7e6554f102558286d4baea3dc3711bc756b
Vince Maina <vchapandrews@gmail.com>
2022-08-12 14:43:33 +0100

    quick test

--- worker.py
+++ worker.py
@@ -1,7 +1,5 @@
 from flask import Flask, request
 
-print('This file has been read.')
-
 worker = Flask(__name__)
 
 @worker.route('/', methods=['POST'])
@@ -9,5 +7,3 @@ def task_handler():
 
     print('Job was queued')
     print(request.values)
-
-    return 'Job was queued - return'
