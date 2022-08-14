10110b7c4d483869cc17a36744762f4b822a4223
Vince Maina <vchapandrews@gmail.com>
2022-08-14 15:19:47 +0100

    Fixed errors

--- beat_store/test_cron.py
+++ beat_store/test_cron.py
@@ -8,18 +8,16 @@ def update_database():
     
     if request.method == 'POST':
 
+        if request.authorization:
+            message = f'Username: {request.authorization.username}, Password: {request.authorization.password}'
+        else:
+            message = 'No authorization added.'
+
         from . mail import send_confirmation_email
 
         response = send_confirmation_email(
             order_id='012345TEST',
-            beat_name=f'''
-                TESTBEATNAME
-                Headers: {request.headers}
-                Args: {request.args}
-                Authorization: {request.authorization}
-                Username: {request.authorization.username}
-                Password: {request.authorization.password}
-                ''',
+            beat_name=message,
             video_title='TESTVIDEOTITLE',
             recipient_address='vince@elevatecopy.com',
             lease_id='1Eny9AaVXVbFvAljb2rD47pP6exeuzvPgoeW5gep-0BY')
