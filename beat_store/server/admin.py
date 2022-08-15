210b6240581d33eba9ef9fb70f155932b039f8b3
Vince Maina <vchapandrews@gmail.com>
2022-08-15 01:22:03 +0100

    Now queing database refresh directly inside admin.py

--- beat_store/server/admin.py
+++ beat_store/server/admin.py
@@ -7,16 +7,35 @@ bp = Blueprint('admin', __name__, url_prefix='/admin')
 def update_database():
 
     if request.method == 'POST':
-        from .. google_api import youtube
-        youtube.add_uploads_to_database()
-  
+
+        import os
+        import redis
+        from rq import Queue
+
+        redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
+        conn = redis.from_url(redis_url)
+        q = Queue(connection=conn)
+
+        from .. google_api.youtube import get_videos, get_audio_links
+        from .. google_api.drive import get_folder_ids
+
+        jobs = q.enqueue_many(
+        [
+            Queue.prepare_data(get_videos, job_id='GET VIDEOS'),
+            Queue.prepare_data(get_audio_links, job_id='GET AUDIO LINKS'),
+            Queue.prepare_data(get_folder_ids, job_id='GET FOLDER IDS'),
+            # Queue.prepare_data(copy_to_Videos, job_id='copy_to_Videos')
+        ]
+        )
+
+
     from .. db import get_db
 
     db = get_db()
 
     cursor = db.cursor()
 
-    data = cursor.execute('SELECT id, title, beat_name, tags, link_to_mixdowns, link_to_stems, link_to_video_audio, thumbnail FROM video').fetchall()
+    data = cursor.execute('SELECT id, title, beat_name, tags, mixdown_folder, stems_folder, link_to_video_audio, thumbnail FROM video').fetchall()
 
     class video():
         def __init__(self, id, title, beat_name, tags, link_to_mixdowns, link_to_stems, link_to_video_audio, thumbnail):
