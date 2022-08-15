e8b89d63660866972dc2034a262a09c61aba3bcc
Vince Maina <vchapandrews@gmail.com>
2022-08-15 01:20:49 +0100

    Updating to use service account authorisation instead

--- beat_store/google_api/credentials/__init__.py
+++ beat_store/google_api/credentials/__init__.py
@@ -1,34 +1,17 @@
-import os
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
+from google.oauth2 import service_account
 
+SCOPES = ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/gmail.send', 'https://www.googleapis.com/auth/documents']
+
+import os
 from dotenv import load_dotenv
 load_dotenv()
 
-BEAT_WEBSITE_OAUTH_CREDENTIALS = os.environ['BEAT_WEBSITE_OAUTH_CREDENTIALS']
-TOKEN_JSON = os.environ.get('TOKEN_JSON')
+SERVICE_ACCOUNT_ENV_VAR = os.environ['SERVICE_ACCOUNT_ENV_VAR']
 
-# If making changes to scope, delete 'token.json'
-SCOPES = ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/gmail.send', 'https://www.googleapis.com/auth/documents']
+with open('service.json', 'w') as f:
+    f.write(SERVICE_ACCOUNT_ENV_VAR)
 
-creds = None
+SERVICE_ACCOUNT_FILE = 'service.json'
 
-if TOKEN_JSON:
-    with open('token.json', 'w') as f: # Creates a credentials.json file from the string in the environment variable, BEAT_WEBSITE_OAUTH_CREDENTIALS. This string was obtained from a json file downlaoded from the google developer console.
-        f.write(TOKEN_JSON)
-
-if os.path.exists('token.json'):
-    creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-# If there are no (valid) credentials available, let the user log in.
-if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-        creds.refresh(Request())
-    else:
-        with open('credentials.json', 'w') as f:
-            f.write(BEAT_WEBSITE_OAUTH_CREDENTIALS)
-        flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-        creds = flow.run_local_server(port=0) # IS THIS GOING TO WORK ON A LIVE SERVER?
-    # Save the credentials for the next run
-    with open('token.json', 'w') as token:
-        token.write(creds.to_json())
+creds = service_account.Credentials.from_service_account_file(
+    SERVICE_ACCOUNT_FILE, scopes=SCOPES)
